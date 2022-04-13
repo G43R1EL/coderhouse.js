807e23fe63c27f273f91884744acdf3787bc0ce1
Nelson Gabriel Hernández <gabrielymarg@gmail.com>
2022-04-12 23:24:03 -0300

    Armado de carrito de compras con arrays y objetos... Funciones para agregar items y quitar... Checkout

--- js/products.js
+++ js/products.js
@@ -3,30 +3,30 @@ const productos = [
     {
         id: '100001',
         imagen: '../assets/no-image.png',
-        titulo: 'Primer producto',
+        titulo: 'Microprocesador Intel i7 9700f',
         descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, rerum!',
-        precio: 1500
+        precio: 32000
     },
     {
         id: '100002',
         imagen: '../assets/no-image.png',
-        titulo: 'Segundo producto',
+        titulo: 'Video ASUS RTX2060 6G',
         descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, rerum!',
-        precio: 2400
+        precio: 95000
     },
     {
         id: '100003',
         imagen: '../assets/no-image.png',
-        titulo: 'Tercer producto',
+        titulo: 'Memoria Kingston HyperX 2666MHz 16GB KIT',
         descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, rerum!',
-        precio: 1800
+        precio: 14000
     },
     {
         id: '100004',
         imagen: '../assets/no-image.png',
-        titulo: 'Cuarto producto',
+        titulo: 'Motherboard Gigabyte H61M DS3H',
         descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, rerum!',
-        precio: 1200
+        precio: 12000
     }
 
 ];
@@ -35,12 +35,12 @@ const productos = [
 function listarProductos() {
     let listaProductosHtml = '';
     for  ( producto of productos ) {
-        let productoHtml = '<div class="product_card" id="pPRODUCT_ID">\n'+
+        let productoHtml = '<div class="product_card" id="PRODUCT_ID">\n'+
                            '<img src="PRODUCT_IMAGE" alt="no_image">\n'+
                            '<h2>PRODUCT_TITLE</h2>\n'+
                            '<p>PRODUCT_DESCRIPTION</p>\n'+
                            '<span><b>Precio:</b> $PRODUCT_PRICE-</span>\n'+
-                           '<button onClick="agregarItem(this.id)" id="PRODUCT_ID">Agregar</button>\n'+
+                           '<button onClick="agregarItem(this.parentNode.id)">Agregar</button><button onClick="eliminarItem(this.parentNode.id)">Eliminar</button>\n'+
                            '</div>';
         productoHtml = productoHtml
             .replace('PRODUCT_ID', producto.id)
@@ -54,25 +54,50 @@ function listarProductos() {
     return listaProductosHtml;
 }
 
-// Agrega items en presupuesto... A desarrollar...
+// Agrega items en carrito...
 function agregarItem(idProducto) {
     let producto = productos.find(prod => prod.id == idProducto);
-    if (producto.precio > 0) {
-        presupuesto = presupuesto + producto.precio;
-        notificacion(idProducto);
+    if (!existe(idProducto)) {
+        carrito.push(producto);
+        console.log('Producto agregado...');
     } else {
-        console.log('No se puede agregar item sin valor');
+        console.log('Ya existe el producto... No agregado.');
     }
 }
 
-// Notifica en consola el producto agregado y el monto del presupuesto... Luego se implementara notificación en el html.
-function notificacion(idProducto) {
-    let producto = productos.find(prod => prod.id == idProducto);
-    console.log('Se agrego el producto: ' + producto.titulo + ' 💸\nEl presupuesto asciende a: $' + presupuesto);
+// Quita items del carrito
+function eliminarItem(idProducto) {
+    if (existe(idProducto)) {
+        let indiceProducto = carrito.findIndex( prod => prod.id == idProducto );
+        carrito.splice(indiceProducto,1);
+        console.log('Producto eliminado...');
+    } else {
+        console.log('El producto no se encuentra en el carrito... Nothing to do here xD');
+    }
 }
 
-// Almacena el monto del presupuesto
-let presupuesto = 0;
+// Verifica si un elemento ya existe en el carrito.
+function existe(idProducto) {
+    if (carrito.find(prod => prod.id == idProducto) === undefined) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+// Muestra lista de productos y total de la cuenta
+function checkOut () {
+    let total = 0;
+    console.log('Producto - Precio');
+    for (producto of carrito) {
+        console.log(producto.titulo + ' - $' + producto.precio);
+        total = total + Number(producto.precio);
+    }
+    console.log('Total : $' + total);
+}
+
+// Carrito de compras
+let carrito = [];
 
 // Obtiene el contenedor de los productos y luego asigna el html generado por la función listarProductos().
 const contenedor = document.getElementById('products_container');
